74bb117b4ff847961a53c5763db00c4c9a63c0e6
Gadoro Zasandalio <70950389+Segnities@users.noreply.github.com>
2022-10-10 11:47:35 +0300

    search component

--- src/components/UI/Search/Search.jsx
+++ src/components/UI/Search/Search.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React from "react";
 
 import { MdSearch } from "react-icons/md";
 
-const Search = () => {
-    return (
-        <div className='search'>
-           <MdSearch className='search-icons' size={'1.3em'}/>
-           <input type='search' placeholder='Type to search...'/>
-        </div>
-    );
+const Search = ({ searhQuery, handleSearchNote }) => {
+  return (
+    <div className="search">
+      <MdSearch className="search-icons" size={"1.3em"} />
+      <input
+        type="search"
+        placeholder="Type to search..."
+        value={searhQuery}
+        onChange={(e) => handleSearchNote(e.target.value)}
+      />
+    </div>
+  );
 };
 
 export default Search;
