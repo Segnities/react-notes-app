19a01d4051bbb12fd5bd71e312f3975cbe38e6c7
Gadoro Zasandalio <70950389+Segnities@users.noreply.github.com>
2022-10-11 16:35:58 +0300

    Delete src directory

--- src/components/utils/getNotes.js
+++
@@ -1,9 +0,0 @@
-export function getNotes() {
-    let localStorageNotes;
-    if(localStorage.getItem('notes-data') === null) {
-        localStorageNotes = [];
-    } else {
-        localStorageNotes = JSON.parse(localStorage.getItem('notes-data'))
-    }
-    return localStorageNotes;
-}
